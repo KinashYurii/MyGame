7c151818f2b8d29b6f5ce0b7b05108a190731dfa
Yurii kinash <ykinash@griddynaics.com>
2018-12-10 18:11:26 +0200

    add project

---
+++ src/test/java/com/griddynamics/rpg/AppTest.java
@@ -0,0 +1,18 @@
+package com.griddynamics.rpg;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void mySecondTest() {
+        assertEquals(2, 1+1);
+    }
+}
